791ca63c5bfc7568fc2f124452baefe3f2a06b80
Ariel Lázaro <ariellazaro444@gmail.com>
2025-01-09 15:29:42 -0400

    Fix navbar link colors

--- main.js
+++ main.js
@@ -1,33 +1,46 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const sections = document.querySelectorAll("section");
-    const links = document.querySelectorAll("nav a");
-    let currentSection = 0, activeLink = 0;
-    
-    links[activeLink].classList.add("active");
-    function scrollToSection(index) {
-        sections[index].scrollIntoView({
-            behavior: "smooth",
-            block: "start",
-        });
-        links.forEach((value, key) => {
-            if (key !== activeLink) links[key].classList.remove("active"); 
-        })
-        links[activeLink].classList.add("active");
-    }
 
-    window.addEventListener("wheel", (event) => {
-        if (event.deltaY > 0 && currentSection < sections.length - 1) {
-            currentSection++;
-            activeLink++;
-        } else if (event.deltaY < 0 && currentSection > 0) {
-            currentSection--;
-            activeLink--;
-        }
-        scrollToSection(currentSection);
+const sections = document.querySelectorAll("section");
+const links = document.querySelectorAll("nav a");
+const contactBtn = document.getElementById("contact-btn");
+
+let currentSection = 0;
+
+links[currentSection].classList.add("active");
+
+function scrollToSection(index) {
+    sections[index].scrollIntoView({
+        behavior: "smooth",
+        block: "start",
     });
+    links.forEach((value, key) => {
+        if (key !== currentSection) links[key].classList.remove("active");
+    });
+    links[currentSection].classList.add("active");
+}
+
+window.addEventListener("wheel", (event) => {
+    if (event.deltaY > 0 && currentSection < sections.length - 1) {
+        currentSection++;
+    } else if (event.deltaY < 0 && currentSection > 0) {
+        currentSection--;
+    }
+    scrollToSection(currentSection);
+});
+    
+document.addEventListener("wheel", e => {
+    e.preventDefault();
+}, {passive: false});
+
+contactBtn.addEventListener("click", e => {
+    currentSection = 4;
+    links.forEach((value, key) => links[key].classList.remove("active"));
 });
 
-document.addEventListener("wheel", ev => {
-    ev.preventDefault();
-    
-}, {passive: false});
+links.forEach((link, key) => {
+    link.addEventListener("click", e => {
+        link.classList.add("active");
+        const otherLinks= Array.from(links).filter(l => l !== link);
+        otherLinks.forEach(l => l.classList.remove("active"));
+        currentSection = key;
+    });
+})
